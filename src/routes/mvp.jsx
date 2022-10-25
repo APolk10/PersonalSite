b71d8f3b853a1c7a7d6c6d318c24850a0a20c72f
Adam Polk <adampolk91@gmail.com>
2022-10-25 15:53:05 -0500

    adds styling to main page and root

--- src/routes/mvp.jsx
+++ src/routes/mvp.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MVP() {
 
   return (
-    <div>
+    <div id="project">
       MVP:
     </div>
   )
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,17 +8,27 @@ import Photography from './photography.jsx';
 export default function Root() {
   const[isEntered, setEntry] = useState(false);
 
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
   return (
-    <>
+    <div>
     {isEntered ?
-      <>
+      <div id="container">
         <div id="sidebar">
-          <h1>Adam</h1>
-          <div>
+          <h1 id="sidebarNameHeader">Adam</h1>
+          <div id="sidebarListHeader">
             My Work
           </div>
           <nav>
-            <ul>
+            <ul id="navbarList">
               <li>
                 <Link to={`/mvp`}>MVP</Link>
               </li>
@@ -32,7 +42,9 @@ export default function Root() {
                 <Link to={`photography`}>Photography</Link>
               </li>
               <li>
-                <Link to={"/"}>Home</Link>
+                <Link to={"/"}>
+                  <img src='https://cdn-icons-png.flaticon.com/16/846/846449.png' />
+                </Link>
               </li>
             </ul>
           </nav>
@@ -40,11 +52,14 @@ export default function Root() {
         <div id="detail">
           <Outlet />
         </div>
-      </> :
-      <div>
+      </div> :
+      <div id="entryButton" /*styles={{
+        backgroundImage:
+        "url('https://images.pexels.com/photos/418831/pexels-photo-418831.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1280&dpr=1')",
+        }}*/>
         <button type="button" onClick={setEntry}>Click Here To Enter </button>
       </div>
     }
-    </>
+    </div>
   );
 }
