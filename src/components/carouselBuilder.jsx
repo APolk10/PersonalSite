8ce7e0907a6405baca08ae95a3843c1bcd98ddf9
Adam Polk <adampolk91@gmail.com>
2022-10-26 09:45:09 -0500

    adds proof of concept for carousel lifecycle

---
+++ src/components/carouselBuilder.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+function PortfolioCarousel({ imageURLs }) {
+  return (
+    <div>
+      Placeholder
+    </div>
+  )
+}
+
+export default PortfolioCarousel;
--- src/routes/mvp.jsx
+++ src/routes/mvp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PortfolioCarousel from '../components/carouselBuilder.jsx';
 
 function MVP() {
 
@@ -6,6 +7,7 @@ function MVP() {
     <div id="project">
       Music By Country App
         <img src="https://i.ibb.co/SBpBSqd/home.jpg" alt="home" border="0"/>
+        <PortfolioCarousel />
     </div>
   )
 }
