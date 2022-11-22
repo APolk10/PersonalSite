09ed07faf0bf659be5a7f3362077734808dfe738
Adam Polk <adampolk91@gmail.com>
2022-11-22 14:57:03 -0600

    adds mostly working linked list structure for graphic

--- src/routes/linkedListGame.jsx
+++ src/routes/linkedListGame.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { useState, setState } from 'react';
 
-// define head
+// define lastInterval to stagger release
+var currentTimeToWait = 0;
 var head;
 
 // build a linked list constructor
 class MonsterSegment {
-  constructor(val) {
-    this.self = val;
+  constructor(randomColor) {
+    this.data = currentTimeToWait;
+    this.color = randomColor
     this.next = null;
   }
 }
 
 // allow for adding segments to monster
-function addSegment(newVal) {
-  let newSegment = new MonsterSegment(newVal);
+function addSegment(color) {
+  currentTimeToWait += .25;
+  let newSegment = new MonsterSegment(color);
   newSegment.next = head;
   head = newSegment;
+  return newSegment;
 }
 
 //
 function LinkedListGame() {
+  const [color, setColor] = useState("purple");
+  const [segments, setSegments] = useState([]); // eventually I want to use an object instead of cheating with array
 
   function onNewMonsterSegmentClick() {
     // create a new value for the node
@@ -29,7 +35,8 @@ function LinkedListGame() {
   }
 
   function addMonsterSegment() {
-
+    const newSegment = addSegment("purple");
+    setSegments(newSegment);
   }
 
   return (
