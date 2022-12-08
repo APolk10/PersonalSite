0f8b7c5a29b69faaddd70c952e66201e60be54fa
Adam Polk <adampolk91@gmail.com>
2022-12-07 21:06:15 -0600

    updates aboutMe page

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -6,13 +6,21 @@ function Home() {
         <div id="aboutMeHeader">Former music teacher explores a new passion...</div>
         <div id="aboutMeContainer">
           <div id="aboutMeBio">
-            <p>Hey there!</p>
+            <p id="aboutMeBioGreeting">Hey there!</p>
             <br></br>
             <p id="aboutMeBioPara">
-            My name is Adam and as the headline says I am a former elementary music educator who is going on a new adventure. I started my coding journey in September of 2021 when a friend and I reconnected and he talked my ear off about his new software job. I had never really thought about a life in tech before that, but from the way he spoke of it I began to get an odd feeling in the pit of my stomach. See, the truth was I had been wondering what was next in my life for a few years now. I love music, and I love education, but I also knew that it was time for a change. From there, I decided to act on that feeling and bought a book about Python. I loved it from the very start. I remember wrestling through setting up my environment for the first time and the first print ('Hello World') and what started as a dip my toes into the pool of software turned into a complete life change as I found myself applying to Hack Reactor by the end of that school year. I had to deep dive into JavaScript to prepare but I was lucky enough to get in and, well, here I am now! Looking to learn and grow in this new and exciting field where I get to build cool things and help improve the world I live in. If you want to know more or just to chat, reach out to me on LinkedIn! I'd love to talk.
+              My name is Adam and I'm a former elementary music educator going on a new adventure. I started my coding journey in 2021 when a friend and I reconnected and he was raving about his new software job. Before that, I had never really thought about the tech field. However, from the way he spoke of it, I began to get an odd feeling in the pit of my stomach.
+            <div>
+              See, the truth was I had been wondering what was next in my life for a few years now. I love music, and I love education, but I also knew that it was time for a change. I decided to act on that feeling and bought a book about Python.
+            <u>I loved it from the very start.</u>
+              I remember wrestling through setting up my environment for the first time and the first print ('Hello World') and what started as a "dip my toes into the pool of software" turned into a complete life change.
+            <p>
+              By the end of that school year I decided to attend Hack Reactor and so I quickly immersed myself in JavaScript. It was tough to prepare, but I was lucky enough to get in and, well, here I am now! Looking to learn and grow in this new and exciting field where I get to build cool things and help improve the world I live in. If you want to know more or just to chat, reach out to me on LinkedIn! I'd love to talk.
+            </p>
+            </div>
             </p>
             <br></br>
-            <p>- Adam</p>
+            <p id="aboutMeBioClosing">Adam</p>
           </div>
           <div>
             <img id="aboutMeHeadshot" src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1670438935/Personal%20Site/IMG_5342_n6cjh6.jpg" width="450vw" height="auto"></img>
