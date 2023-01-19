be2a1b7f00b9e2d1f28b8cf9307923e2d44a6a9a
Adam Polk <adampolk91@gmail.com>
2023-01-19 11:13:21 -0600

    cleans root route and adds component to be called with removed code

---
+++ src/components/sideBar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SideBar = function({ openAndCloseNav }) {
+  return (
+    <div id="mySidebar" className="sidebar">
+      <h1 id="sidebarNameHeader">Adam's</h1>
+      <div id="sidebarListHeader">
+        Portfolio
+      </div>
+      <nav>
+        <ul id="navbarList">
+          <li id="listName">
+            <Link id="listBlock" to={'/home'} onClick={openAndCloseNav}>About Me</Link>
+          </li>
+          <li id="listName">
+            <Link id="listBlock" to={`/mvp`} onClick={openAndCloseNav}>
+              <p id="listText">Music By</p>
+              <p id="listText">Country</p>
+              <p id="listText">Application</p>
+            </Link>
+          </li>
+          <li id="listName">
+            <Link id="listBlock" to={`sdc`} onClick={openAndCloseNav}>
+              <p id="listText">Server and</p>
+              <p id="listText">Database</p>
+              <p id="listText">Overhaul</p>
+            </Link>
+          </li>
+          <li id="listName">
+            <Link id="listBlock" to={`blueOcean`} onClick={openAndCloseNav}>
+              <p id="listText">Construction</p>
+              <p id="listText">Job Portal</p>
+              <p id="listText">Website</p>
+            </Link>
+          </li>
+          <li id="listName">
+            <Link id="listBlock" to={`photography`} onClick={openAndCloseNav}>
+              <p id="listText">McMillan</p>
+              <p id="listText">Photography</p>
+              </Link>
+          </li>
+          <li id="listName">
+            <Link id="listBlock" to={`linkedListGame`} onClick={openAndCloseNav}>
+              <p id="listText">Linked List</p>
+              <p id="listText">Game</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default SideBar;
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import Home from './home.jsx';
+import SideBar from '../components/sideBar.jsx';
 import MVP from './mvp.jsx';
 import SDC from './sdc.jsx';
 import BlueOcean from './blueOcean.jsx';
@@ -72,53 +73,8 @@ export default function Root(props) {
   return (
         <div id="root">
           <div id="container">
-            <div id="mySidebar" className="sidebar">
-              <h1 id="sidebarNameHeader">Adam's</h1>
-              <div id="sidebarListHeader">
-                Portfolio
-              </div>
-              <nav>
-                <ul id="navbarList">
-                  <li id="listName">
-                    <Link id="listBlock" to={'/home'} onClick={openAndCloseNav}>About Me</Link>
-                  </li>
-                  <li id="listName">
-                    <Link id="listBlock" to={`/mvp`} onClick={openAndCloseNav}>
-                      <p id="listText">Music By</p>
-                      <p id="listText">Country</p>
-                      <p id="listText">Application</p>
-                    </Link>
-                  </li>
-                  <li id="listName">
-                    <Link id="listBlock" to={`sdc`} onClick={openAndCloseNav}>
-                      <p id="listText">Server and</p>
-                      <p id="listText">Database</p>
-                      <p id="listText">Overhaul</p>
-                    </Link>
-                  </li>
-                  <li id="listName">
-                    <Link id="listBlock" to={`blueOcean`} onClick={openAndCloseNav}>
-                      <p id="listText">Construction</p>
-                      <p id="listText">Job Portal</p>
-                      <p id="listText">Website</p>
-                    </Link>
-                  </li>
-                  <li id="listName">
-                    <Link id="listBlock" to={`photography`} onClick={openAndCloseNav}>
-                      <p id="listText">McMillan</p>
-                      <p id="listText">Photography</p>
-                      </Link>
-                  </li>
-                  <li id="listName">
-                    <Link id="listBlock" to={`linkedListGame`} onClick={openAndCloseNav}>
-                      <p id="listText">Linked List</p>
-                      <p id="listText">Game</p>
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
             <div id="main">
+              <SideBar openAndCloseNav={openAndCloseNav}></SideBar>
               <button id="menubtn" className="openbtn" onClick={openAndCloseNav}>&#9776; Menu</button>
               <div id="detail">
                 <Outlet />
