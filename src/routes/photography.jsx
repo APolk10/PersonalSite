0b7ba083462f039cdb6e0a3e917cb837ba673311
Adam Polk <adampolk91@gmail.com>
2023-05-10 12:34:28 -0500

    updates photography page and fixes a bug where the outercontainer was set to flex row and no height was set (small pages would not fit the page height)

--- src/routes/photography.jsx
+++ src/routes/photography.jsx
@@ -3,20 +3,22 @@ import React from 'react';
 
 function Photography() {
   return (
-    <div id="photography-outer-container">
-      <div id="photography-container">
-          <div id="photography-header">
-            McMillan Photography
+    <div className="outerContainer">
+      <div className="pageContainer">
+        <div className="pageTitle">
           <a target="_blank" rel="noreferrer" href="https://github.com/APolk10/TimPhotography">
               <img id="mvp-githubLink" src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1670455086/Personal%20Site/github-mark_g6mb0a.png"></img>
-            </a>
+          </a>
+          <p>McMillan Photography</p>
+        </div>
+        <div className="dataContainer">
+          <div id="photography-placeholder">Coming Soon!</div>
+          <div>
+            <img id='sdc-gif' src='https://res.cloudinary.com/dbvgiuj6d/image/upload/v1683060372/Personal%20Site/photographySite_jfzx3y.gif'></img>
           </div>
-        <div id="photography-placeholder">Coming Soon!</div>
-        <div>
-          <img className='photographyGif' src='https://res.cloudinary.com/dbvgiuj6d/image/upload/v1683060372/Personal%20Site/photographySite_jfzx3y.gif'></img>
+          {/* <TechStackCards technologies={['NextJS']}>          </TechStackCards> */}
         </div>
       </div>
-        {/* <TechStackCards technologies={['NextJS']}>          </TechStackCards> */}
     </div>
   )
 }
