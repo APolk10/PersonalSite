d8d4b03fbe9ba2415747a20278548f1304a854a9
Adam Polk <adampolk91@gmail.com>
2023-05-17 10:36:16 -0500

    updates Linked List page

--- src/routes/linkedListGame.jsx
+++ src/routes/linkedListGame.jsx
@@ -91,29 +91,31 @@ function LinkedListGame() {
       <div className="pageContainer">
         <div className="pageTitle">Linked List Monster!</div>
         <div className="dataContainer">
+          <div className="textHeader" id="specialHeaderStart">What is this?</div>
           <p className="subTextAlt">I created this as a visual aid for linked lists as I found many online representations less interesting to look at.</p>
+          <p className="subTextAlt">The basic premise is that this animation is made using a linked list under the hood. Each segment is connected to the neighboring righthand segment. As you click new segments, new segments are inserted to the tail end of this &quot;monster&quot; and the monster gets longer as a result. I wanted to visualize a linked list and I think this simple animation does the trick rather well.</p>
           <p className="subTextAlt">
-            I got the core idea from a game I had played called Terraria. In this game there is a boss who resembles a segmented worm. When fighting this worm, each link can be attacked and broken individually, creating new and indpendent worms. One became two and so the subdivision would continue, until the boss was defeated.
+            I got the core idea from a game I had played called Terraria. In this game there is a boss named Eater of Worlds who resembles a segmented worm. When fighting this worm, each link can be attacked and broken individually, creating new and indpendent worms. One became two and so the subdivision would continue, until the boss was defeated.
           </p>
           <p className="subTextAlt">Below is my representation using vanilla CSS, JavaScript and React.</p>
-          <p className="subTextAlt">(Best viewed on desktop or in landscape mode)</p>
           <div id="ll-container">
             <div id="ll-gamebox">
               <div>
                 {segmentContainer.length > 0 ? segmentContainer.map(segment =>
                   <div key={`segmentPath-${segment.data}`} className={`segmentPath-${segment.data}`}>
                     <div key={`segment-${segment.data}`} className={`segment-${segment.data}`} style={{'backgroundColor': segment.color}} color={segment.color} data={segment.data}  onClick={handleSegmentClick}>
-
                   </div>
                   </div>
                 ) : <></>}
               </div>
             </div >
+            <p className="subTextAlt">(Best viewed on desktop or in landscape mode)</p>
             <div id="ll-buttonBox">
               <button className="ll-buttons" type="button" onClick={resetAnimation}>Reset worm</button>
               <button className="ll-buttons" type="button" onClick={addMonsterSegment}>Add segment</button>
             </div>
-            <p className="subTextAlt">The above worm uses a linked list class structure and I wrote the functions for this page to iterate through the linked list as well as randomize all but the first color of the segments. The cap for segments is 10 and although this is not the fully fledged Terraria boss, it does interact with you a bit if you are brave enough to click a segment!</p>
+            <p className="subTextAlt">I did this by writing a linked list class and I wrote the functions for this page to iterate through the linked list as well as randomize all but the first color of the segments. Each segment, or node, contains the time to wait before rendering, the unique color associated with it, and a reference to the neighboring node that renders after it.</p>
+            <p className="subTextAlt">The cap for segments is 10 and although this is not the fully fledged Terraria boss, it does interact with you a bit if you are brave enough to click a segment!</p>
           </div>
         </div>
         </div>
